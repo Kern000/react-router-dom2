b56e69009e1c81eb79a6612e9611aa4a9883f2ec
unknown <holupkern@gmail.com>
2023-07-24 17:23:53 +0800

    first commit - completed simple react router practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AboutUs from './pages/AboutUs';
+import ContactUs from './pages/ContactUs';
+import LandingPage from './pages/LandingPage';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App(){
+  return(
+    <React.Fragment>
+      <Router>
+        <nav className="navbar navbar-expand-sm bg-light">
+          <ul className="navbar-nav">
+            <li className="nav-item ms-3">
+              <Link to="/" className="nav-link"> Home </Link>
+            </li>
+            <li className="nav-item ms-3">
+              <Link to="/aboutus" className="nav-link"> About Us </Link>
+            </li>
+            <li className="nav-item ms-3">
+              <Link to="/contactus" className="nav-link"> Contact Us </Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+            <Route path="/" element={<LandingPage/>} />
+            <Route path="/aboutus" element={<AboutUs/>} />
+            <Route path="/contactus" element={<ContactUs/>} />
+        </Routes>
+      </Router>
+    </React.Fragment>
+  )
 }
-
-export default App;
---
+++ src/pages/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function LandingPage(props) {
+    return(
+        <React.Fragment>
+            <h1>Landing Page</h1>
+            <blockquote> Welcome to the website </blockquote>
+        </React.Fragment>
+    )
+}
+
